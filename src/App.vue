8fc064bc49045975c1bd1655016e363f41a8c8db
alexyzhou <alexy.zhou@outlook.com>
2023-11-08 20:55:35 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
   </suspense>
 </template>
 
+<style>
+html, body, #app {
+  @apply w-full h-full overflow-hidden;
+}
+</style>
+
 <script setup>
 console.log('👋 This message is being logged by "App.vue", included via Vite');
 </script>
